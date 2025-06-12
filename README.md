94d1d481b0cfc1860ff19bc4ec5ab5b95cfddb8d
Bolazcoding <bolajiadelabu0@gmail.com>
2025-06-12 11:14:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# Frontend Mentor - Age calculator app solution
+
+This is a solution to the [Age calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+Users should be able to:
+
+- View an age in years, months, and days after submitting a valid date through the form
+- Receive validation errors if:
+  - Any field is empty when the form is submitted
+  - The day number is not between 1-31
+  - The month number is not between 1-12
+  - The date is in the future
+  - The date is invalid e.g. 31/04/1991 (there are 30 days in April)
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+### Screenshot
+
+- Preview
+
+![](./assets/images/Desktop%20preview.png)
+
+![](./assets/images/Field%20required.png)
+
+![](./assets/images/Valid%20day.png)
+
+![](./assets/images/In%20the%20past.png)
+
+### Links
+
+- Solution URL: [Solution URL](https://github.com/Bolazcoding/age-calculator-app)
+- Live Site URL: [Live site URL](https://josedev-advice-generator-app.netlify.app/)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Preprocessor, SCSS
+- JavaScript
+
+### What I learned
+
+- Working with dates is difficult, but i worked it out.
+
+### Continued development
+
+This project tested my skills but I'm looking forward to more projects to build in the future.
+
+## Author
+
+- Website - [my github](https://github.com/Bolazcoding)
+- Frontend Mentor - [@myusername](https://www.frontendmentor.io/profile/Bolazcoding)
+- Twitter - [@myusername](https://x.com/_joseDev_)
+
+## Acknowledgments
+
+This project was completed by me.
---
+++ index.js
@@ -0,0 +1,164 @@
+'use strict';
+
+const input_day = document.querySelector('#input__day');
+const input_month = document.querySelector('#input__month');
+const input_year = document.querySelector('#input__year');
+const labelDay = document.querySelector('.label__day');
+const labelMonth = document.querySelector('.label__month');
+const labelYear = document.querySelector('.label__year');
+const requiredDay = document.querySelector('.error-required-day');
+const requiredMonth = document.querySelector('.error-required-month');
+const requiredYear = document.querySelector('.error-required-year');
+const errorDay = document.querySelector('.error-day');
+const errorMonth = document.querySelector('.error-month');
+const errorYear = document.querySelector('.error-year');
+const outputDay = document.querySelector('.output__day-input');
+const outputMonth = document.querySelector('.output__month-input');
+const outputYear = document.querySelector('.output__year-input');
+
+const calcAgeBtn = document.querySelector('.logo-validate');
+
+function validateAgeForm() {
+  // this is the day validation
+  const day = input_day.value;
+  input_day.value = input_day.value.replace(/[^0-9]/g, '');
+
+  if (day === '' || input_day.value !== day) {
+    labelDay.style.color = 'hsl(0, 100%, 67%)';
+    requiredDay.style.display = 'block';
+    input_day.style.border = '1px solid hsl(0, 100%, 67%)';
+    // return false;
+  } else if (day === 0 || day > 31 || day < 0) {
+    labelDay.style.color = 'hsl(0, 100%, 67%)';
+    errorDay.style.display = 'block';
+    input_day.style.border = '1px solid hsl(0, 100%, 67%)';
+    return false;
+  } else {
+    errorDay.style.display = 'none';
+    requiredDay.style.display = 'none';
+    labelDay.style.color = 'hsl(0, 1%, 44%)';
+    input_day.style.border = '1px solid hsl(0, 0%, 86%)';
+  }
+
+  // this is the month validation
+  const month = input_month.value;
+  input_month.value = input_month.value.replace(/[^0-9]/g, '');
+
+  if (month === '') {
+    labelMonth.style.color = 'hsl(0, 100%, 67%)';
+    requiredMonth.style.display = 'block';
+    input_month.style.border = '1px solid hsl(0, 100%, 67%)';
+    // return false;
+  } else if (month === 0 || month > 12 || month < 0) {
+    labelMonth.style.color = 'hsl(0, 100%, 67%)';
+    errorMonth.style.display = 'block';
+    input_month.style.border = '1px solid hsl(0, 100%, 67%)';
+    return false;
+  } else {
+    errorMonth.style.display = 'none';
+    requiredMonth.style.display = 'none';
+    labelMonth.style.color = 'hsl(0, 1%, 44%)';
+    input_month.style.border = '1px solid hsl(0, 0%, 86%)';
+  }
+
+  // this is the year validation
+  const year = input_year.value;
+  input_year.value = input_year.value.replace(/[^0-9]/g, '');
+  const now = new Date();
+  const yearNow = now.getFullYear();
+
+  if (year === '') {
+    labelYear.style.color = 'hsl(0, 100%, 67%)';
+    requiredYear.style.display = 'block';
+    input_year.style.border = '1px solid hsl(0, 100%, 67%)';
+    return false;
+  } else if (year === 0 || year > yearNow || year < 0) {
+    labelYear.style.color = 'hsl(0, 100%, 67%)';
+    errorYear.style.display = 'block';
+    input_year.style.border = '1px solid hsl(0, 100%, 67%)';
+    return false;
+  } else if (year.length !== 4) {
+    labelYear.style.color = 'hsl(0, 100%, 67%)';
+    errorYear.style.display = 'block';
+    input_year.style.border = '1px solid hsl(0, 100%, 67%)';
+    return false;
+  } else {
+    errorYear.style.display = 'none';
+    requiredYear.style.display = 'none';
+    labelYear.style.color = 'hsl(0, 1%, 44%)';
+    input_year.style.border = '1px solid hsl(0, 0%, 86%)';
+  }
+
+  calculateAge(day, month, year);
+  isValidDate(day, month, year);
+
+  // console.log(yearNow);
+  // console.log(day);
+}
+
+function calculateAge(day, month, year) {
+  const dob = new Date(year, month - 1, day);
+  const now = new Date();
+
+  let ageInYears = now.getFullYear() - dob.getFullYear();
+  let ageInMonths = now.getMonth() - dob.getMonth();
+  let ageInDays = now.getDate() - dob.getDate();
+
+  // Adjust for negative days
+  if (ageInDays < 0) {
+    ageInMonths -= 1;
+
+    // Get days in previous month
+    const previousMonth = new Date(now.getFullYear(), now.getMonth(), 0);
+    ageInDays += previousMonth.getDate();
+  }
+
+  // Adjust for negative months
+  if (ageInMonths < 0) {
+    ageInYears -= 1;
+    ageInMonths += 12;
+  }
+
+  outputDay.textContent = `${ageInDays}`;
+  outputMonth.textContent = `${ageInMonths}`;
+  outputYear.textContent = `${ageInYears}`;
+
+  // console.log(`${ageInYears} years`);
+  // console.log(`${ageInMonths} months`);
+  // console.log(`${ageInDays} days`);
+}
+
+function invalidDateDetails() {
+  outputDay.textContent = '- -';
+  outputMonth.textContent = '- -';
+  outputYear.textContent = '- -';
+}
+
+function isValidDate(day, month, year) {
+  // month is 1-indexed for this function
+  const daysInMonth = new Date(year, month, 0).getDate();
+
+  if (day > daysInMonth) {
+    errorDay.style.display = 'block';
+    invalidDateDetails();
+    return false;
+  } else {
+    errorDay.style.display = 'none';
+  }
+}
+
+// making sure there are no letters and border is showing
+
+input_day.addEventListener('input', function () {
+  input_day.value = input_day.value.replace(/[^0-9]/g, '');
+});
+
+input_month.addEventListener('input', function () {
+  input_month.value = input_month.value.replace(/[^0-9]/g, '');
+});
+
+input_year.addEventListener('input', function () {
+  input_year.value = input_year.value.replace(/[^0-9]/g, '');
+});
+
+calcAgeBtn.addEventListener('click', validateAgeForm);
